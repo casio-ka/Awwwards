158f91a090e7d27dc5f82e05fa4bb8837e4f0ae2
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-01 19:50:06 +0300

    Update apps.py
    Added verbose name and imported signals

--- project/apps.py
+++ project/apps.py
@@ -1,5 +1,9 @@
 from django.apps import AppConfig
-
+from django.utils.translation import ugettext_lazy as _
 
 class ProjectConfig(AppConfig):
     name = 'project'
+    verbose_name = ('project')
+
+    def ready(self):
+        import project.signals
