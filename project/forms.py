f162f1a39bc0fd1c7fd98ab2765725f2f3f43250
casio-ka <32677770+casio-ka@users.noreply.github.com>
2020-11-03 02:14:02 +0300

    Update Forms
    Added Rateform

--- project/forms.py
+++ project/forms.py
@@ -3,21 +3,14 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
-from .models import Post, Profile
+from .models import Post, Profile,Rate
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(max_length=254)
+    email = forms.EmailField(max_length=80)
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2',)
-
-class PostForm(forms.ModelForm):
-    picture = CloudinaryField('image')
-
-    class Meta:
-        model = Post
-        fields = ('picture', 'name', 'website', 'description',) 
 
 class UpdateUserForm(forms.ModelForm):
     email = forms.EmailField(max_length=254)
@@ -25,8 +18,23 @@ class UpdateUserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'email')
+class PostForm(forms.ModelForm):
+    picture = CloudinaryField('image')
+
+    class Meta:
+        model = Post
+        fields = ('picture', 'projectname', 'website', 'description',) 
 
 class UpdateProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ['name', 'country', 'prof_pic', 'bio']
+        fields = ('name', 'country', 'prof_pic', 'bio')
+
+class RateForm(forms.ModelForm):
+    design = forms.ChoiceField(choices=[(x, x) for x in range(0, 11)])
+    usability = forms.ChoiceField(choices=[(x, x) for x in range(0, 11)])
+    content = forms.ChoiceField(choices=[(x, x) for x in range(0, 11)])
+    creativity = forms.ChoiceField(choices=[(x, x) for x in range(0, 11)])
+    class Meta:
+        model =Rate
+        fields= ('design','usability','content','creativity')
